edd188e51d1a0aad138c7d2b11d94185bf1221c6
dreambigatall <halidmahammed.16.hm@gmail.com>
2025-05-13 09:33:45 +0300

    db modify

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-  mongoose.connect(process.env.DATABASE_LOCAL, { useNewUrlParser: true, useUnifiedTopology: true })
+  mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => {
       console.error(err);
